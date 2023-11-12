47ecf9290a31e3cdc70a859a02fa929cd7f47929
moshibar <25570708+MoshiBar@users.noreply.github.com>
2023-11-12 00:31:40 -0600

    mutant standard

--- packages/frontend/src/components/global/MkEmoji.vue
+++ packages/frontend/src/components/global/MkEmoji.vue
@@ -11,7 +11,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 
 <script lang="ts" setup>
 import { computed, inject } from 'vue';
-import { char2twemojiFilePath, char2fluentEmojiFilePath } from '@/scripts/emoji-base.js';
+import { char2twemojiFilePath, char2fluentEmojiFilePath, char2mutantStandardFilePath } from '@/scripts/emoji-base.js';
 import { defaultStore } from '@/store.js';
 import { getEmojiName } from '@/scripts/emojilist.js';
 import * as os from '@/os.js';
@@ -26,7 +26,7 @@ const props = defineProps<{
 
 const react = inject<((name: string) => void) | null>('react', null);
 
-const char2path = defaultStore.state.emojiStyle === 'twemoji' ? char2twemojiFilePath : char2fluentEmojiFilePath;
+const char2path = defaultStore.state.emojiStyle === 'mutant' ? char2mutantStandardFilePath : (defaultStore.state.emojiStyle === 'twemoji' ? char2twemojiFilePath : char2fluentEmojiFilePath);
 
 const useOsNativeEmojis = computed(() => defaultStore.state.emojiStyle === 'native');
 const url = computed(() => {
--- packages/frontend/src/scripts/emoji-base.ts
+++ packages/frontend/src/scripts/emoji-base.ts
@@ -5,6 +5,7 @@
 
 const twemojiSvgBase = '/twemoji';
 const fluentEmojiPngBase = '/fluent-emoji';
+const mutantEmojiSvgBase = 'https://cdn.ambrosia.moe/mtnt-code-svg';
 
 export function char2twemojiFilePath(char: string): string {
 	let codes = Array.from(char, x => x.codePointAt(0)?.toString(16));
@@ -22,4 +23,14 @@ export function char2fluentEmojiFilePath(char: string): string {
 	codes = codes.filter(x => x && x.length);
 	const fileName = codes.map(x => x!.padStart(4, '0')).join('-');
 	return `${fluentEmojiPngBase}/${fileName}.png`;
+}
+
+export function char2mutantStandardFilePath(char: string): string {
+	let codes = Array.from(char, x => x.codePointAt(0)?.toString(16));
+	// Fluent Emojiは国旗非対応 https://github.com/microsoft/fluentui-emoji/issues/25
+	if (codes[0]?.startsWith('1f1')) return char2twemojiFilePath(char);
+	if (!codes.includes('200d')) codes = codes.filter(x => x !== 'fe0f');
+	codes = codes.filter(x => x && x.length);
+	const fileName = codes.map(x => x!.padStart(4, '0')).join('-');
+	return `${mutantEmojiSvgBase}/${fileName}.png`;
 }
